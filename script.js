553e4ccc84dec42d73f76f8ff7858cdaad3e1462
upadhyaykartikeya <ukartik451@gmail.com>
2025-05-11 18:25:29 +0530

    tweet button function

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ const api_url = "https://api.breakingbadquotes.xyz/v1/quotes/5";
 async function getquote(url){
     const response = await fetch(url);
     var data = await response.json();
-    console.log(data);
     quote.innerHTML = data[0].quote;
     author.innerHTML = data[0].author;
 }
 
 getquote(api_url);
+
+function tweet(){
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "---by" + author.innerHTML, "Tweet Window", "width=600, height=300")
+}
